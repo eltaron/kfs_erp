f39556f85472b30063d731bdfc414bdba49159a3
unknown <eltaroon1082000@gmail.com>
2025-12-17 14:59:27 +0200

    init

---
+++ MyERP.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyERP.Web.Data
+{
+    // بنورث من IdentityDbContext عشان ينزل جداول المستخدمين (Users, Roles, Logins)
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // هنا بنعرف الجداول الخاصة بينا مستقبلاً
+        // public DbSet<Employee> Employees { get; set; }
+    }
+}
